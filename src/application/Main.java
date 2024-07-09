cd9b3596f20570496800e70130a3765d29157cd7
Rafael Silva <rafaelmike33@gmail.com>
2024-07-09 12:52:28 -0300

    Exercise done

---
+++ src/application/Main.java
@@ -0,0 +1,62 @@
+package application;
+
+import entities.Product;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.DoubleConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        System.out.println(sourceFolderStr);
+
+        boolean success = new File(sourceFolderStr + "/out").mkdir();
+
+        System.out.println("Folder created: " + success);
+
+        String targetFileStr = sourceFolderStr + "/out/summary.csv";
+
+        try (BufferedReader br  = new BufferedReader(new FileReader(sourceFileStr))) {
+            String itemCsv = br.readLine();
+            while(itemCsv != null){
+                String[] fields = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+                for(Product item: list) {
+                    bw.write(item.getName() + "," + String.format("%.2f", item.total()));
+                    bw.newLine();
+                }
+                System.out.println(targetFileStr + " Created");
+            } catch (IOException e) {
+                System.out.println("Error writing file: " + e.getMessage());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+
+}
